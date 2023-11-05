eb84047b88c73d6be39ae06b9d9043eb4c31e046
inhasid <sidorchuk.inga@gmail.com>
2023-11-05 11:18:14 +0100

    player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function savePlaybackTime(currentTime) {
+    localStorage.setItem('videoplayer-current-time', currentTime)
+}
+
+player.on('timeupdate', throttle((data) => {
+    savePlaybackTime(data.seconds);
+}, 1000));
+
+function restorePlaybackTime() {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime) {
+        player.setCurrentTime(parseFloat(savedTime));
+    }
+}
+
+player.ready().then(() => {
+    restorePlaybackTime();
+});
